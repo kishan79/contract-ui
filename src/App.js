c908eeed940ce1922d8efb022e0cec1c48eb9ff9
Kishan <kishan.237@gmail.com>
2022-06-13 17:12:36 +0530

    Added wallet support

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React, { Component } from "react";
 import API from "./apis";
+import Web3 from 'web3';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      privatekey: "",
       network: "https://rinkeby.infura.io/v3/c1a5906ba0104dc9894f2b4c28aa0b2c",
       address: "",
+      account:""
     };
   }
 
-  handleInputChange = (e) => {
-    this.setState({ privatekey: e.target.value });
+  connectWallet = async () => {
+    try {
+      let web3 = new Web3(Web3.givenProvider);
+      if (typeof window.web3 != 'undefined') {
+        web3 = new Web3(window.ethereum);
+      } else {
+        return { status: false };
+      }
+      let account = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      return { status: true, account: account[0] };
+    } catch (error) {
+      console.log('error', error?.message);
+      return { status: false };
+    }
   };
+
+  onConnectWallet = async () => {
+    if (window.ethereum) {
+      let response = await this.connectWallet();
+      if (response.status) {
+        this.setState({account: response.account});
+        alert('Successfully connected!');
+      } else {
+        alert('Something went wrong!');
+      }
+    } else {
+      alert(
+        'Please install Metamask wallet extension to use this app.'
+      );
+      setTimeout(() => {
+        let popup = window.open('https://metamask.io/', '_blank');
+        if (popup) {
+          popup.focus();
+        } else {
+          alert(
+            'You have disabled popups. Please enable the popups or follow this link: https://metamask.io/'
+          );
+        }
+      }, 3000);
+    }
+  };
+
 
   handleSelectChange = (e) => {
     this.setState({ network: e.target.value });
   };
 
   handleSubmit = (event) => {
-    if (this.state.privatekey.length && this.state.network.length) {
+    if (this.state.network.length) {
       API.post("/deploy", this.state)
         .then((res) => {
           const { data } = res;
@@ -36,19 +78,16 @@ class App extends Component {
   };
 
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <h4>Deploy smart contract to blockchain</h4>
+        <button
+          onClick={this.onConnectWallet}
+        >
+          Connect wallet
+        </button>
         <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            id="privatekey"
-            value={this.state.privatekey}
-            onChange={this.handleInputChange}
-            required
-            placeholder="Enter wallet private key"
-          />
-
           <label htmlFor="network">Choose a testnet network:</label>
 
           <select
--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const SERVER_URI = 'https://contract-ser.herokuapp.com/api';
+// export const SERVER_URI = 'https://contract-ser.herokuapp.com/api';
+export const SERVER_URI = 'http://localhost:4000/api';
